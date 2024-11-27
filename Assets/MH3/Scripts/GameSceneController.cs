c578e9a3a5684cd68962c133276a8ef76a344f02
hiroki-kitahara <hk.mp5k.a4.pdw83@gmail.com>
2024-11-27 11:28:31 +0900

    instantiate

--- Assets/MH3/Scripts/GameSceneController.cs
+++ Assets/MH3/Scripts/GameSceneController.cs
@@ -1,16 +1,16 @@
-using System;
-using Cysharp.Threading.Tasks;
+using MH3.ActorControllers;
 using UnityEngine;
 
 namespace MH3
 {
     public class GameSceneController : MonoBehaviour
     {
-        private async void Start()
+        [SerializeField]
+        private Actor playerPrefab;
+        
+        private void Start()
         {
-            await UniTask.DelayFrame(1);
-            Debug.Log("GameSceneController Start");
-            throw new Exception("Test");
+            Instantiate(playerPrefab);
         }
     }
 }
