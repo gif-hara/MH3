15c06f0b5b0cb9bbeab76acaefdad690594bf0b7
hiroki-kitahara <hk.mp5k.a4.pdw83@gmail.com>
2024-11-27 14:26:50 +0900

    GoogleSpreadSheetDownloader

---
+++ Assets/MH3/Scripts/GoogleSpreadSheetDownloader.cs
@@ -0,0 +1,29 @@
+using Cysharp.Threading.Tasks;
+using UnityEngine.Networking;
+
+namespace HK
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public sealed class GoogleSpreadSheetDownloader
+    {
+        const string url = "https://script.google.com/macros/s/AKfycbwqWHRGzk8_ajxPxNFvJB9u9vsUHkHBHc12VkOizHfsOOyCXuLr8OprvScKN8u6yx7ZVQ/exec";
+
+        public static async UniTask<string> DownloadAsync(string sheetName)
+        {
+            var request = UnityWebRequest.Get(url + "?sheetName=" + sheetName);
+            await request.SendWebRequest();
+            if (request.result != UnityWebRequest.Result.Success)
+            {
+                // エラー処理
+                UnityEngine.Debug.LogError(request.error);
+                return null;
+            }
+            else
+            {
+                return request.downloadHandler.text;
+            }
+        }
+    }
+}
