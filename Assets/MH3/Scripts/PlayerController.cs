cf3fb992c9bc700bfea9d11ba883d94d7ca8243b
hiroki-kitahara <hk.mp5k.a4.pdw83@gmail.com>
2024-11-27 14:17:58 +0900

    PlayerController

--- Assets/MH3/Scripts/PlayerController.cs
+++ Assets/MH3/Scripts/PlayerController.cs
@@ -1,5 +1,4 @@
 using MH3.ActorControllers;
-using UnityEngine;
 
 namespace MH3
 {
@@ -7,7 +6,6 @@ namespace MH3
     {
         public static void Attach(Actor actor, InputController inputController)
         {
-            Debug.Log($"PlayerController.Attach: {actor}");
         }
     }
 }
