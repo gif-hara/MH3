46ea2f4a6c36e61261f8c3c1c321f75ebdac4f58
hiroki-kitahara <hk.mp5k.a4.pdw83@gmail.com>
2024-11-27 14:51:12 +0900

    MovementController

--- Assets/MH3/Scripts/ActorControllers/ActorMovementController.cs
+++ Assets/MH3/Scripts/ActorControllers/ActorMovementController.cs
@@ -1,6 +1,35 @@
+using R3;
+using R3.Triggers;
+using UnityEngine;
+
 namespace MH3.ActorControllers
 {
     public class ActorMovementController
     {
+        private Vector3 velocity;
+        
+        private Vector3 lastVelocity;
+
+        public void Setup(Actor actor)
+        {
+            actor.UpdateAsObservable()
+                .Subscribe(actor, (_, a) =>
+                {
+                    a.transform.position += velocity * Time.deltaTime;
+                    lastVelocity = velocity;
+                    velocity = Vector3.zero;
+                })
+                .RegisterTo(actor.destroyCancellationToken);
+        }
+        
+        public void Move(Vector3 velocity)
+        {
+            this.velocity = velocity;
+        }
+        
+        public bool IsMoving()
+        {
+            return lastVelocity != Vector3.zero;
+        }
     }
 }
