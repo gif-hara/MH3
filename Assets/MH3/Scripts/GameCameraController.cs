5d39b1c3c098a78d6d8a1b85848fbc8f675dd6d0
hiroki-kitahara <hk.mp5k.a4.pdw83@gmail.com>
2024-11-29 15:14:51 +0900

    GameCameraController

--- Assets/MH3/Scripts/GameCameraController.cs
+++ Assets/MH3/Scripts/GameCameraController.cs
@@ -1,8 +1,16 @@
+using Unity.Cinemachine;
 using UnityEngine;
 
 namespace MH3
 {
     public class GameCameraController : MonoBehaviour
     {
+        [SerializeField]
+        private CinemachineCamera defaultCinemachineCamera;
+        
+        public void SetTrackingTarget(Transform target)
+        {
+            defaultCinemachineCamera.Target.TrackingTarget = target;
+        }
     }
 }
