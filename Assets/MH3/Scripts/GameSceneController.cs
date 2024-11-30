f75f7c57d146d2d71de06e9625e93b5101bf1233
gif-hara <hk.mp5k.a4.pdw83@gmail.com>
2024-11-30 13:29:40 +0900

    Add AudioManager prefab to GameSceneController and register it on start

--- Assets/MH3/Scripts/GameSceneController.cs
+++ Assets/MH3/Scripts/GameSceneController.cs
@@ -14,11 +14,15 @@ namespace MH3
 
         [SerializeField]
         private GameCameraController gameCameraControllerPrefab;
+
+        [SerializeField]
+        private AudioManager audioManagerPrefab;
         
         private void Start()
         {
             TinyServiceLocator.RegisterAsync(new InputController(), destroyCancellationToken).Forget();
             TinyServiceLocator.RegisterAsync(masterData, destroyCancellationToken).Forget();
+            TinyServiceLocator.RegisterAsync(Instantiate(audioManagerPrefab), destroyCancellationToken).Forget();
             var player = Instantiate(playerPrefab);
             var gameCameraController = Instantiate(gameCameraControllerPrefab);
             gameCameraController.SetTrackingTarget(player.transform);
