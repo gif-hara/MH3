c80c32325144e93f65a7be40fa374f5bed088640
gif-hara <hk.mp5k.a4.pdw83@gmail.com>
2024-11-30 22:44:15 +0900

    Add extension method to set layer recursively for GameObjects

---
+++ Assets/MH3/Scripts/Extensions.GameObject.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+namespace MH3
+{
+    public static partial class Extensions
+    {
+        public static void SetLayerRecursively(this GameObject gameObject, int layer)
+        {
+            gameObject.layer = layer;
+            foreach (Transform child in gameObject.transform)
+            {
+                child.gameObject.SetLayerRecursively(layer);
+            }
+        }
+    }
+}
