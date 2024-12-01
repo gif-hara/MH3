4c2e890889a5b2b2b0e0f231617cd2c33608fbec
gif-hara <hk.mp5k.a4.pdw83@gmail.com>
2024-12-01 19:33:23 +0900

    Update EnemyController to enable targeting of player during enemy attacks

--- Assets/MH3/Scripts/EnemyController.cs
+++ Assets/MH3/Scripts/EnemyController.cs
@@ -1,17 +1,22 @@
 using MH3.ActorControllers;
 using R3;
 using R3.Triggers;
+using UnityEngine;
 
 namespace MH3
 {
     public class EnemyController
     {
-        public static void Attach(Actor actor)
+        public static void Attach(Actor actor, Actor target)
         {
             actor.UpdateAsObservable()
-                .Subscribe(actor, (_, a) =>
+                .Subscribe((actor, target), (_, t) =>
                 {
-                    a.AttackController.TryAttack();
+                    var (actor, target) = t;
+                    var lookAt = target.transform.position - actor.transform.position;
+                    lookAt.y = 0.0f;
+                    actor.MovementController.Rotate(Quaternion.LookRotation(lookAt));
+                    actor.AttackController.TryAttack();
                 })
                 .RegisterTo(actor.destroyCancellationToken);
         }
