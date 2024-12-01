1b4d6c1269a217a20c4f5feb3dddb9d41cff2b03
gif-hara <hk.mp5k.a4.pdw83@gmail.com>
2024-12-01 14:46:38 +0900

    Add Actor specifications to MasterData for player and enemy characters

--- Assets/MH3/Scripts/MasterData.cs
+++ Assets/MH3/Scripts/MasterData.cs
@@ -3,9 +3,11 @@ using System.Linq;
 using System.Threading;
 using Cysharp.Threading.Tasks;
 using HK;
+using MH3.ActorControllers;
 using R3;
 using UnityEditor;
 using UnityEngine;
+using UnitySequencerSystem;
 
 namespace MH3
 {
@@ -22,6 +24,10 @@ namespace MH3
         [SerializeField]
         private AttackSpec.DictionaryList attackSpecs;
         public AttackSpec.DictionaryList AttackSpecs => attackSpecs;
+
+        [SerializeField]
+        private ActorSpec.DictionaryList actorSpecs;
+        public ActorSpec.DictionaryList ActorSpecs => actorSpecs;
 
 #if UNITY_EDITOR
         [ContextMenu("Update")]
@@ -42,15 +48,22 @@ namespace MH3
             {
                 "WeaponSpec",
                 "AttackSpec",
+                "ActorSpec",
             };
             var database = await UniTask.WhenAll(
                 masterDataNames.Select(GoogleSpreadSheetDownloader.DownloadAsync)
             );
             weaponSpecs.Set(JsonHelper.FromJson<WeaponSpec>(database[0]));
             attackSpecs.Set(JsonHelper.FromJson<AttackSpec>(database[1]));
+            actorSpecs.Set(JsonHelper.FromJson<ActorSpec>(database[2]));
             foreach (var weaponSpec in weaponSpecs.List)
             {
                 weaponSpec.ModelData = AssetDatabase.LoadAssetAtPath<WeaponModelData>($"Assets/MH3/Database/WeaponModelData/{weaponSpec.Id}.asset");
+            }
+            foreach (var actorSpec in actorSpecs.List)
+            {
+                actorSpec.AttackSequence = AssetDatabase.LoadAssetAtPath<ScriptableSequences>($"Assets/MH3/Database/StateSequences/State.Attack.{actorSpec.AttackSequenceKey}.asset");
+                actorSpec.ActorPrefab = AssetDatabase.LoadAssetAtPath<Actor>($"Assets/MH3/Prefabs/Actor.{actorSpec.ActorPrefabKey}.prefab");
             }
             EditorUtility.SetDirty(this);
             AssetDatabase.SaveAssets();
@@ -88,6 +101,36 @@ namespace MH3
 
             [Serializable]
             public class DictionaryList : DictionaryList<int, AttackSpec>
+            {
+                public DictionaryList() : base(x => x.Id)
+                {
+                }
+            }
+        }
+
+        [Serializable]
+        public class ActorSpec
+        {
+            public int Id;
+
+            public int HitPoint;
+
+            public float MoveSpeed;
+
+            public float RotationSpeed;
+
+            public int WeaponId;
+
+            public string AttackSequenceKey;
+
+            public string ActorPrefabKey;
+
+            public ScriptableSequences AttackSequence;
+
+            public Actor ActorPrefab;
+
+            [Serializable]
+            public class DictionaryList : DictionaryList<int, ActorSpec>
             {
                 public DictionaryList() : base(x => x.Id)
                 {
