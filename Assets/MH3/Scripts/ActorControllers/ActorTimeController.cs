0227e8905c476a2c75be1628d7c8ea6a363f2fe5
gif-hara <hk.mp5k.a4.pdw83@gmail.com>
2024-12-01 14:09:17 +0900

    Enhance ActorTimeController to support hit stop functionality and manage time scale stack

--- Assets/MH3/Scripts/ActorControllers/ActorTimeController.cs
+++ Assets/MH3/Scripts/ActorControllers/ActorTimeController.cs
@@ -1,3 +1,6 @@
+using System;
+using System.Collections.Generic;
+using Cysharp.Threading.Tasks;
 using HK;
 using MH3.ActorControllers;
 using R3;
@@ -7,12 +10,37 @@ namespace MH3
 {
     public class ActorTimeController
     {
+        private readonly Actor actor;
+
         public HK.Time Time { get; } = new HK.Time(HK.Time.Root);
 
         public Observable<Unit> UpdatedTimeScale => Observable.FromEvent(h => Time.UpdatedTimeScale += h, h => Time.UpdatedTimeScale -= h);
 
+        private readonly Stack<float> timeScaleStack = new();
+
         public ActorTimeController(Actor actor)
         {
+            this.actor = actor;
+            PushTimeScale(1.0f);
+        }
+
+        public async UniTask BeginHitStopAsync(float timeScale, float duration)
+        {
+            PushTimeScale(timeScale);
+            await UniTask.Delay(TimeSpan.FromSeconds(duration), cancellationToken: actor.destroyCancellationToken);
+            PopTimeScale();
+        }
+
+        public void PushTimeScale(float timeScale)
+        {
+            timeScaleStack.Push(timeScale);
+            Time.timeScale = timeScale;
+        }
+
+        public void PopTimeScale()
+        {
+            timeScaleStack.Pop();
+            Time.timeScale = timeScaleStack.Peek();
         }
     }
 }
