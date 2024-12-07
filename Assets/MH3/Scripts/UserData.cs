c6c52aee50785e6c20d684641800351c1520685a
gif-hara <hk.mp5k.a4.pdw83@gmail.com>
2024-12-08 01:04:59 +0900

    Add UserData class to manage instance weapon data

---
+++ Assets/MH3/Scripts/UserData.cs
@@ -0,0 +1,10 @@
+using System.Collections.Generic;
+
+namespace MH3
+{
+    public class UserData
+    {
+        private List<InstanceWeaponData> instanceWeaponDataList = new();
+        public List<InstanceWeaponData> InstanceWeaponDataList => instanceWeaponDataList;
+    }
+}
