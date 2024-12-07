a45490943933658df16abd1908a493b586691970
gif-hara <hk.mp5k.a4.pdw83@gmail.com>
2024-12-07 19:36:33 +0900

    Add UIViewHomeMenu script for managing home menu UI interactions

---
+++ Assets/MH3/Scripts/UIViewHomeMenu.cs
@@ -0,0 +1,35 @@
+using System.Threading;
+using Cysharp.Threading.Tasks;
+using HK;
+using MH3.ActorControllers;
+using R3;
+using TMPro;
+
+namespace MH3
+{
+    public class UIViewHomeMenu
+    {
+        public static async UniTask OpenAsync(HKUIDocument headerDocumentPrefab, HKUIDocument listDocumentPrefab, Actor actor, CancellationToken scope)
+        {
+            var homeMenuScope = new CancellationDisposable(CancellationTokenSource.CreateLinkedTokenSource(scope));
+            var header = UnityEngine.Object.Instantiate(headerDocumentPrefab);
+            var stateMachine = new TinyStateMachine();
+
+            // 待機
+            {
+                await UniTask.WaitUntilCanceled(homeMenuScope.Token);
+            }
+
+            // 終了処理
+            {
+                homeMenuScope.Dispose();
+                stateMachine.Dispose();
+            }
+
+            void SetHeaderText(string text)
+            {
+                header.Q<TMP_Text>("Header").text = text;
+            }
+        }
+    }
+}
