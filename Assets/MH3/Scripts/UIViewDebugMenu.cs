555f071282c699ca86d980d5b4405e0385ab3ff7
gif-hara <hk.mp5k.a4.pdw83@gmail.com>
2024-12-07 17:43:11 +0900

    Refactor UIViewDebugMenu to improve state management and input handling during debug menu operations

--- Assets/MH3/Scripts/UIViewDebugMenu.cs
+++ Assets/MH3/Scripts/UIViewDebugMenu.cs
@@ -12,15 +12,32 @@ namespace MH3
     {
         public static async UniTask OpenAsync(HKUIDocument listDocumentPrefab, CancellationToken scope)
         {
+            var debugMenuScope = new CancellationDisposable(CancellationTokenSource.CreateLinkedTokenSource(scope));
             var tempTimeScale = HK.Time.Root.timeScale;
             HK.Time.Root.timeScale = 0.0f;
             var inputController = TinyServiceLocator.Resolve<InputController>();
             inputController.PushActionType(InputController.InputActionType.UI);
-            var debugMenuScope = new CancellationDisposable(CancellationTokenSource.CreateLinkedTokenSource(scope));
-            var list = UIViewList.CreateWithPages(
-                listDocumentPrefab,
-                new List<Action<HKUIDocument>>
-                {
+            var stateMachine = new TinyStateMachine();
+            stateMachine.Change(StateRoot);
+
+            // 待機
+            {
+                await UniTask.WaitUntilCanceled(debugMenuScope.Token);
+            }
+
+            // 終了処理
+            {
+                inputController.PopActionType();
+                HK.Time.Root.timeScale = tempTimeScale;
+                stateMachine.Dispose();
+            }
+
+            async UniTask StateRoot(CancellationToken scope)
+            {
+                var list = UIViewList.CreateWithPages(
+                    listDocumentPrefab,
+                    new List<Action<HKUIDocument>>
+                    {
                     document =>
                     {
                         UIViewList.ApplyAsSimpleElement(document, "Debug1", _ => Debug.Log("Debug1"));
@@ -33,16 +50,18 @@ namespace MH3
                     {
                         UIViewList.ApplyAsSimpleElement(document, "Debug3", _ => Debug.Log("Debug3"));
                     },
-                },
-                0
-                );
-            await UniTask.WaitUntilCanceled(debugMenuScope.Token);
-            if (list != null)
-            {
-                UnityEngine.Object.Destroy(list.gameObject);
+                    },
+                    0
+                    );
+                inputController.Actions.UI.Cancel.OnPerformedAsObservable()
+                    .Subscribe(_ => debugMenuScope.Dispose())
+                    .RegisterTo(scope);
+                await UniTask.WaitUntilCanceled(scope);
+                if (list != null)
+                {
+                    UnityEngine.Object.Destroy(list.gameObject);
+                }
             }
-            inputController.PopActionType();
-            HK.Time.Root.timeScale = tempTimeScale;
         }
     }
 }
