4b09184ae408ba6e19a96ac876550f5cd76411e3
gif-hara <hk.mp5k.a4.pdw83@gmail.com>
2024-12-07 16:50:17 +0900

    Implement input action type management in InputController

--- Assets/MH3/Scripts/InputController.cs
+++ Assets/MH3/Scripts/InputController.cs
@@ -1,13 +1,51 @@
+using System.Collections.Generic;
+
 namespace MH3
 {
     public class InputController
     {
+        public enum InputActionType
+        {
+            Player,
+            UI,
+        }
+
         public MHInputActions Actions { get; }
-        
+
+        private readonly Stack<InputActionType> actionTypes = new();
+
         public InputController()
         {
             Actions = new MHInputActions();
+            PushActionType(InputActionType.Player);
             Actions.Enable();
+        }
+
+        public void PushActionType(InputActionType type)
+        {
+            actionTypes.Push(type);
+            ChangeInputType(type);
+        }
+
+        public void PopActionType()
+        {
+            actionTypes.Pop();
+            ChangeInputType(actionTypes.Peek());
+        }
+
+        private void ChangeInputType(InputActionType type)
+        {
+            Actions.Player.Disable();
+            Actions.UI.Disable();
+            switch (type)
+            {
+                case InputActionType.Player:
+                    Actions.Player.Enable();
+                    break;
+                case InputActionType.UI:
+                    Actions.UI.Enable();
+                    break;
+            }
         }
     }
 }
