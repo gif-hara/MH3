40186553b3cae68fa727814e55667cddb4021f0f
gif-hara <hk.mp5k.a4.pdw83@gmail.com>
2024-12-08 16:25:55 +0900

    Add InstanceWeaponViewCritical sequence to display weapon critical data

---
+++ Assets/MH3/Scripts/UnitySequencerSystem/Sequences/InstanceWeaponViewCritical.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Threading;
+using Cysharp.Threading.Tasks;
+using TMPro;
+using UnityEngine;
+using UnitySequencerSystem;
+
+namespace MH3
+{
+    [Serializable]
+    public class InstanceWeaponViewCritical : Sequence
+    {
+        [SerializeField]
+        private string instanceWeaponDataKey;
+
+        [SerializeField]
+        private TMP_Text text;
+
+        public override UniTask PlayAsync(Container container, CancellationToken cancellationToken)
+        {
+            var instanceWeaponData = container.Resolve<InstanceWeaponData>(instanceWeaponDataKey);
+            text.text = instanceWeaponData.Critical.ToString("0%");
+            return UniTask.CompletedTask;
+        }
+    }
+}
