698c4331648142b6d2fc15d576104debaebe5eb5
gif-hara <hk.mp5k.a4.pdw83@gmail.com>
2024-12-08 18:39:18 +0900

    Add InstanceSkillCoreFactory to create InstanceSkillCore instances

---
+++ Assets/MH3/Scripts/InstanceSkillCoreFactory.cs
@@ -0,0 +1,22 @@
+using System.Collections.Generic;
+using HK;
+using UnityEngine;
+
+namespace MH3
+{
+    public static class InstanceSkillCoreFactory
+    {
+        public static InstanceSkillCore Create(int skillCoreSpecId)
+        {
+            var skillCoreSpec = TinyServiceLocator.Resolve<MasterData>().SkillCoreSpecs.Get(skillCoreSpecId);
+            var skillCoreCount = skillCoreSpec.GetSkillCoreCounts().Lottery(x => x.Weight);
+            var skills = new List<InstanceSkill>();
+            for (var i = 0; i < skillCoreCount.Count; i++)
+            {
+                var skillCoreEffect = skillCoreSpec.GetSkillCoreEffects().Lottery(x => x.Weight);
+                skills.Add(new InstanceSkill(skillCoreEffect.SkillType, skillCoreEffect.Level, skillCoreEffect.RareType));
+            }
+            return new InstanceSkillCore(skillCoreSpecId, skills);
+        }
+    }
+}
