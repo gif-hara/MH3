9c67b98e9b961ab1fed1ae092786ab93d56a6d6f
gif-hara <hk.mp5k.a4.pdw83@gmail.com>
2024-12-08 14:53:38 +0900

    Add InstanceWeaponViewAttack and InstanceWeaponViewWeaponName sequences for weapon data display

---
+++ Assets/MH3/Scripts/UnitySequencerSystem/Sequences/InstanceWeaponViewAttack.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Threading;
+using Cysharp.Threading.Tasks;
+using TMPro;
+using UnityEngine;
+using UnitySequencerSystem;
+
+namespace MH3
+{
+    [Serializable]
+    public class InstanceWeaponViewAttack : Sequence
+    {
+        [SerializeField]
+        private string instanceWeaponDataKey;
+
+        [SerializeField]
+        private TMP_Text text;
+
+        public override UniTask PlayAsync(Container container, CancellationToken cancellationToken)
+        {
+            var instanceWeaponData = container.Resolve<InstanceWeaponData>(instanceWeaponDataKey);
+            text.text = instanceWeaponData.Attack.ToString();
+            return UniTask.CompletedTask;
+        }
+    }
+}
---
+++ Assets/MH3/Scripts/UnitySequencerSystem/Sequences/InstanceWeaponViewWeaponName.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Threading;
+using Cysharp.Threading.Tasks;
+using TMPro;
+using UnityEngine;
+using UnitySequencerSystem;
+
+namespace MH3
+{
+    [Serializable]
+    public class InstanceWeaponViewWeaponName : Sequence
+    {
+        [SerializeField]
+        private string instanceWeaponDataKey;
+
+        [SerializeField]
+        private TMP_Text text;
+
+        public override UniTask PlayAsync(Container container, CancellationToken cancellationToken)
+        {
+            var instanceWeaponData = container.Resolve<InstanceWeaponData>(instanceWeaponDataKey);
+            text.text = instanceWeaponData.WeaponSpec.Name;
+            return UniTask.CompletedTask;
+        }
+    }
+}
