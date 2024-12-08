205641abebffbe65e281884996ef0ae4af83dfb4
gif-hara <hk.mp5k.a4.pdw83@gmail.com>
2024-12-08 17:24:57 +0900

    Add instanceSkillCoreList to UserData for managing skill core instances

--- Assets/MH3/Scripts/UserData.cs
+++ Assets/MH3/Scripts/UserData.cs
@@ -1,15 +1,28 @@
+using System;
 using System.Collections.Generic;
+using UnityEngine;
 
 namespace MH3
 {
+    [Serializable]
     public class UserData
     {
+        [SerializeField]
         private List<InstanceWeaponData> instanceWeaponDataList = new();
         public List<InstanceWeaponData> InstanceWeaponDataList => instanceWeaponDataList;
+
+        [SerializeField]
+        private List<InstanceSkillCore> instanceSkillCoreList = new();
+        public List<InstanceSkillCore> InstanceSkillCoreList => instanceSkillCoreList;
 
         public void AddInstanceWeaponData(InstanceWeaponData instanceWeaponData)
         {
             instanceWeaponDataList.Add(instanceWeaponData);
+        }
+
+        public void AddInstanceSkillCore(InstanceSkillCore instanceSkillCore)
+        {
+            instanceSkillCoreList.Add(instanceSkillCore);
         }
     }
 }
