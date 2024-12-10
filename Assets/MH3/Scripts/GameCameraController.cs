15e91e05c3ec9c50e05db4d181734cbea82d624f
gif-hara <hk.mp5k.a4.pdw83@gmail.com>
2024-12-11 00:11:53 +0900

    Add support for multiple Cinemachine cameras in GameCameraController

--- Assets/MH3/Scripts/GameCameraController.cs
+++ Assets/MH3/Scripts/GameCameraController.cs
@@ -1,3 +1,5 @@
+using System;
+using HK;
 using Unity.Cinemachine;
 using UnityEngine;
 
@@ -9,13 +11,37 @@ namespace MH3
         private CinemachineCamera defaultCinemachineCamera;
 
         [SerializeField]
+        private CinemachineCameraElement.DictionaryList cinemachineCameras;
+        public CinemachineCameraElement.DictionaryList CinemachineCameras => cinemachineCameras;
+
+        [SerializeField]
         private Camera controlledCamera;
         public Camera ControlledCamera => controlledCamera;
+
+        public Transform DefaultCinemachineCameraTrackingTarget => defaultCinemachineCamera.Target.TrackingTarget;
+
+        public Transform DefaultCinemachineCameraLookAtTarget => defaultCinemachineCamera.Target.LookAtTarget;
 
         public void SetTrackingTarget(Transform tracking, Transform lookAt)
         {
             defaultCinemachineCamera.Target.TrackingTarget = tracking;
             defaultCinemachineCamera.Target.LookAtTarget = lookAt;
+        }
+
+        [Serializable]
+        public class CinemachineCameraElement
+        {
+            [SerializeField]
+            private CinemachineCamera cinemachineCamera;
+            public CinemachineCamera CinemachineCamera => cinemachineCamera;
+
+            [Serializable]
+            public class DictionaryList : DictionaryList<string, CinemachineCameraElement>
+            {
+                public DictionaryList() : base(x => x.cinemachineCamera.name)
+                {
+                }
+            }
         }
     }
 }
