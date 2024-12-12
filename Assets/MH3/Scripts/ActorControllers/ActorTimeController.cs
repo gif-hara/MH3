8b5e6e95e73f1e1254d2c70e77c4add67691daf9
gif-hara <hk.mp5k.a4.pdw83@gmail.com>
2024-12-12 22:59:10 +0900

    Refactor ActorTimeController to streamline hit stop functionality and remove unused time scale stack

--- Assets/MH3/Scripts/ActorControllers/ActorTimeController.cs
+++ Assets/MH3/Scripts/ActorControllers/ActorTimeController.cs
@@ -1,5 +1,3 @@
-using System;
-using System.Collections.Generic;
 using Cysharp.Threading.Tasks;
 using HK;
 using MH3.ActorControllers;
@@ -16,35 +14,14 @@ namespace MH3
 
         public Observable<Unit> UpdatedTimeScale => Observable.FromEvent(h => Time.UpdatedTimeScale += h, h => Time.UpdatedTimeScale -= h);
 
-        private readonly Stack<float> timeScaleStack = new();
-
         public ActorTimeController(Actor actor)
         {
             this.actor = actor;
-            PushTimeScale(1.0f);
         }
 
-        public async UniTask BeginHitStopAsync(float timeScale, float duration)
+        public UniTask BeginHitStopAsync(float timeScale, float duration)
         {
-            if (duration <= 0)
-            {
-                return;
-            }
-            PushTimeScale(timeScale);
-            await UniTask.Delay(TimeSpan.FromSeconds(duration), cancellationToken: actor.destroyCancellationToken);
-            PopTimeScale();
-        }
-
-        public void PushTimeScale(float timeScale)
-        {
-            timeScaleStack.Push(timeScale);
-            Time.timeScale = timeScale;
-        }
-
-        public void PopTimeScale()
-        {
-            timeScaleStack.Pop();
-            Time.timeScale = timeScaleStack.Peek();
+            return Time.BeginHitStopAsync(duration, timeScale, actor.destroyCancellationToken);
         }
     }
 }
