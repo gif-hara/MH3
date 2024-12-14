36af679bc8151ed553a9c781365c69848f42eb39
gif-hara <hk.mp5k.a4.pdw83@gmail.com>
2024-12-14 19:13:03 +0900

    Add Spawn method to Projectile for instantiating projectiles

--- Assets/MH3/Scripts/ProjectileControllers/Projectile.cs
+++ Assets/MH3/Scripts/ProjectileControllers/Projectile.cs
@@ -4,5 +4,10 @@ namespace MH3.ProjectileControllers
 {
     public class Projectile : MonoBehaviour
     {
+        public Projectile Spawn(Vector3 position, Quaternion rotation)
+        {
+            var projectile = Instantiate(this, position, rotation);
+            return projectile;
+        }
     }
 }
