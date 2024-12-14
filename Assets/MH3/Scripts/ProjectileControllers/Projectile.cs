7fb800c74262999707d2f7c4900e029e7ebff2c4
gif-hara <hk.mp5k.a4.pdw83@gmail.com>
2024-12-14 20:45:41 +0900

    Refactor Projectile class to include owner and attack specifications in Spawn method

--- Assets/MH3/Scripts/ProjectileControllers/Projectile.cs
+++ Assets/MH3/Scripts/ProjectileControllers/Projectile.cs
@@ -1,13 +1,26 @@
+using MH3.ActorControllers;
 using UnityEngine;
 
 namespace MH3.ProjectileControllers
 {
-    public class Projectile : MonoBehaviour
+    public class Projectile : MonoBehaviour, IActorOnTriggerEnterEvent
     {
-        public Projectile Spawn(Vector3 position, Quaternion rotation)
+        private Actor owner;
+
+        private MasterData.AttackSpec attackSpec;
+
+        public Projectile Spawn(Actor owner, MasterData.AttackSpec attackSpec, Vector3 position, Quaternion rotation)
         {
             var projectile = Instantiate(this, position, rotation);
+            projectile.owner = owner;
+            projectile.attackSpec = attackSpec;
+            projectile.gameObject.SetLayerRecursively(owner.GetProjectileLayer());
             return projectile;
+        }
+
+        public void Influence(Actor target, Collider collider)
+        {
+            target.SpecController.TakeDamage(owner, attackSpec, collider.ClosestPoint(transform.position), 1.0f);
         }
     }
 }
