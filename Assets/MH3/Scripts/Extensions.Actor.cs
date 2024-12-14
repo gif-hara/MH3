010e3e813d0ebc497e0fb95c9fa09c933c5524f6
gif-hara <hk.mp5k.a4.pdw83@gmail.com>
2024-12-14 20:37:20 +0900

    Add GetProjectileLayer method to handle projectile layer mapping

--- Assets/MH3/Scripts/Extensions.Actor.cs
+++ Assets/MH3/Scripts/Extensions.Actor.cs
@@ -11,7 +11,17 @@ namespace MH3
             {
                 Layer.ActorPlayer => (int)Layer.ActorPlayerAttack,
                 Layer.ActorEnemy => (int)Layer.ActorEnemyAttack,
-                _ => throw new System.NotImplementedException(),
+                _ => throw new System.NotImplementedException($"Layer: {actor.gameObject.layer}"),
+            };
+        }
+
+        public static int GetProjectileLayer(this Actor actor)
+        {
+            return (Layer)actor.gameObject.layer switch
+            {
+                Layer.ActorPlayer => (int)Layer.ProjectilePlayer,
+                Layer.ActorEnemy => (int)Layer.ProjectileEnemy,
+                _ => throw new System.NotImplementedException($"Layer: {actor.gameObject.layer}"),
             };
         }
     }
