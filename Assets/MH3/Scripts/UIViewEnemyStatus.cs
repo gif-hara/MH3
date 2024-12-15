e24595deb75bafe7621a368d99b54eda2c764178
gif-hara <hk.mp5k.a4.pdw83@gmail.com>
2024-12-15 10:25:04 +0900

    Add UIViewEnemyStatus class for enemy health UI management

---
+++ Assets/MH3/Scripts/UIViewEnemyStatus.cs
@@ -0,0 +1,36 @@
+using System.Threading;
+using Cysharp.Threading.Tasks;
+using HK;
+using MH3.ActorControllers;
+using R3;
+using UnityEngine.UI;
+
+namespace MH3
+{
+    public class UIViewEnemyStatus
+    {
+        private readonly HKUIDocument document;
+
+        public UIViewEnemyStatus(HKUIDocument documentPrefab, CancellationToken scope)
+        {
+            document = UnityEngine.Object.Instantiate(documentPrefab);
+            scope.RegisterWithoutCaptureExecutionContext(() =>
+            {
+                document.DestroySafe();
+            });
+        }
+
+        public void BeginObserve(Actor actor)
+        {
+            document.gameObject.SetActive(actor.SpecController.VisibleStatusUI);
+            actor.SpecController.HitPoint
+                .Subscribe(document, (_, d) =>
+                {
+                    d.Q<HKUIDocument>("Slider.HitPoint")
+                        .Q<Slider>("Slider")
+                        .value = (float)actor.SpecController.HitPoint.CurrentValue / actor.SpecController.HitPointMax.CurrentValue;
+                })
+                .RegisterTo(actor.destroyCancellationToken);
+        }
+    }
+}
