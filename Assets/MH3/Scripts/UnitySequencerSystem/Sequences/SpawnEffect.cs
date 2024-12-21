6434669f95deaaff477d759ebdd0daeddb82a091
gif-hara <hk.mp5k.a4.pdw83@gmail.com>
2024-12-21 12:59:22 +0900

    Add SpawnEffect sequence class for effect management in Unity sequencer

---
+++ Assets/MH3/Scripts/UnitySequencerSystem/Sequences/SpawnEffect.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Threading;
+using Cysharp.Threading.Tasks;
+using HK;
+using UnityEngine;
+using UnitySequencerSystem;
+using UnitySequencerSystem.Resolvers;
+
+namespace MH3
+{
+    [Serializable]
+    public class SpawnEffect : Sequence
+    {
+        [SerializeReference, SubclassSelector]
+        private StringResolver effectKeyResolver;
+
+        [SerializeReference, SubclassSelector]
+        private StringResolver registerKeyResolver;
+
+        public override UniTask PlayAsync(Container container, CancellationToken cancellationToken)
+        {
+            var effect = TinyServiceLocator.Resolve<EffectManager>().Rent(effectKeyResolver.Resolve(container));
+            container.Register(registerKeyResolver.Resolve(container), effect.transform);
+            return UniTask.CompletedTask;
+        }
+    }
+}
