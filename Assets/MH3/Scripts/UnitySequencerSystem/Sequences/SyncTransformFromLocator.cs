f3b935016c24ebbfca845088152e4cc6f0f11b4d
gif-hara <hk.mp5k.a4.pdw83@gmail.com>
2024-12-21 13:00:09 +0900

    Add SyncTransformFromLocator sequence for synchronizing transform properties from a locator

---
+++ Assets/MH3/Scripts/UnitySequencerSystem/Sequences/SyncTransformFromLocator.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Threading;
+using Cysharp.Threading.Tasks;
+using HK;
+using MH3.UnitySequencerSystem.Resolvers;
+using UnityEngine;
+using UnitySequencerSystem;
+using UnitySequencerSystem.Resolvers;
+
+namespace MH3
+{
+    [Serializable]
+    public class SyncTransformFromLocator : Sequence
+    {
+        [SerializeReference, SubclassSelector]
+        private TransformResolver targetTransformResolver;
+
+        [SerializeReference, SubclassSelector]
+        private ActorResolver actorResolver;
+
+        [SerializeReference, SubclassSelector]
+        private StringResolver locatorKeyResolver;
+
+        public override UniTask PlayAsync(Container container, CancellationToken cancellationToken)
+        {
+            var target = targetTransformResolver.Resolve(container);
+            var actor = actorResolver.Resolve(container);
+            var locator = actor.LocatorHolder.Get(locatorKeyResolver.Resolve(container));
+            target.position = locator.position;
+            target.rotation = locator.rotation;
+            target.localScale = locator.localScale;
+            return UniTask.CompletedTask;
+        }
+    }
+}
