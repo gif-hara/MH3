37ec8c1b2dcd6bbba78e02e3f7939d738178d85b
gif-hara <hk.mp5k.a4.pdw83@gmail.com>
2024-12-26 23:15:09 +0900

    Add SystemData class for managing audio volume settings

---
+++ Assets/MH3/Scripts/SystemData.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+
+namespace MH3
+{
+    public class SystemData
+    {
+        [SerializeField]
+        private float masterVolume = 1.0f;
+
+        [SerializeField]
+        private float bgmVolume = 1.0f;
+
+        [SerializeField]
+        private float sfxVolume = 1.0f;
+
+        public float MasterVolume
+        {
+            get => masterVolume;
+            set => masterVolume = value;
+        }
+
+        public float BgmVolume
+        {
+            get => bgmVolume;
+            set => bgmVolume = value;
+        }
+
+        public float SfxVolume
+        {
+            get => sfxVolume;
+            set => sfxVolume = value;
+        }
+    }
+}
