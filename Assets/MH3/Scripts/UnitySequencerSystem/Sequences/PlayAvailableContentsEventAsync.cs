576f476197b28b0bee4c426f1f0360d7916c2a0e
gif-hara <hk.mp5k.a4.pdw83@gmail.com>
2024-12-30 21:31:20 +0900

    Add PlayAvailableContentsEventAsync sequence for handling available content events

---
+++ Assets/MH3/Scripts/UnitySequencerSystem/Sequences/PlayAvailableContentsEventAsync.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Threading;
+using Cysharp.Threading.Tasks;
+using HK;
+using UnityEngine;
+using UnitySequencerSystem;
+
+namespace MH3
+{
+    [Serializable]
+    public class PlayAvailableContentsEventAsync : Sequence
+    {
+        [SerializeField]
+        private Define.AvaiableContentsEventTrigger trigger;
+
+        public override async UniTask PlayAsync(Container container, CancellationToken cancellationToken)
+        {
+            var userData = TinyServiceLocator.Resolve<UserData>();
+            var availableContentsEvents = TinyServiceLocator.Resolve<MasterData>().AvailableContentsEvents.Get(trigger);
+            foreach (var availableContentsEvent in availableContentsEvents)
+            {
+                if (userData.AvailableContents.NewContents.Contains(availableContentsEvent.NewContentsKey))
+                {
+                    var sequences = availableContentsEvent.Sequences.Sequences;
+                    var sequencer = new Sequencer(container, sequences);
+                    await sequencer.PlayAsync(cancellationToken);
+                    userData.AvailableContents.RemoveNewContent(availableContentsEvent.NewContentsKey);
+                    SaveSystem.Save(TinyServiceLocator.Resolve<SaveData>(), SaveData.Path);
+                }
+            }
+        }
+    }
+}
