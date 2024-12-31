1a7a7e069383071a048c56175a273ef61f63764b
gif-hara <hk.mp5k.a4.pdw83@gmail.com>
2024-12-31 09:38:43 +0900

    Refactor ISkill and Skill classes to consolidate parameter selector logic

--- Assets/MH3/Scripts/Skills/ISkill.cs
+++ Assets/MH3/Scripts/Skills/ISkill.cs
@@ -9,15 +9,9 @@ namespace MH3.SkillSystems
 
         int Level { get; }
 
-        float GetParameter(Define.ActorParameterType type, Actor owner, Actor target);
-
-        int GetParameterInt(Define.ActorParameterType type, Actor owner, Actor target);
-
         float GetParameter(Define.ActorParameterType type, Actor owner);
 
         int GetParameterInt(Define.ActorParameterType type, Actor owner);
-
-        ISkill RegisterParameterSelector(Define.ActorParameterType type, Func<Actor, Actor, float> selector);
 
         ISkill RegisterParameterSelector(Define.ActorParameterType type, Func<Actor, float> selector);
     }
--- Assets/MH3/Scripts/Skills/Skill.cs
+++ Assets/MH3/Scripts/Skills/Skill.cs
@@ -1,14 +1,13 @@
 using System;
 using System.Collections.Generic;
 using MH3.ActorControllers;
+using UnityEngine;
 
 namespace MH3.SkillSystems
 {
     public sealed class Skill : ISkill
     {
-        private readonly Dictionary<Define.ActorParameterType, Func<Actor, float>> parameterOwnerOnlySelectors = new();
-
-        private readonly Dictionary<Define.ActorParameterType, Func<Actor, Actor, float>> parameterOwnerTargetSelectors = new();
+        private readonly Dictionary<Define.ActorParameterType, Func<Actor, float>> parameterSelectors = new();
 
         public Define.SkillType SkillType { get; private set; }
 
@@ -20,43 +19,23 @@ namespace MH3.SkillSystems
             Level = level;
         }
 
-        public float GetParameter(Define.ActorParameterType type, Actor owner, Actor target)
-        {
-            if (parameterOwnerTargetSelectors.ContainsKey(type))
-            {
-                return parameterOwnerTargetSelectors[type](owner, target);
-            }
-            return 0.0f;
-        }
-
-        public int GetParameterInt(Define.ActorParameterType type, Actor owner, Actor target)
-        {
-            return (int)GetParameter(type, owner, target);
-        }
-
         public float GetParameter(Define.ActorParameterType type, Actor owner)
         {
-            if (parameterOwnerOnlySelectors.ContainsKey(type))
+            if (parameterSelectors.ContainsKey(type))
             {
-                return parameterOwnerOnlySelectors[type](owner);
+                return parameterSelectors[type](owner);
             }
             return 0.0f;
         }
 
         public int GetParameterInt(Define.ActorParameterType type, Actor owner)
         {
-            return (int)GetParameter(type, owner);
-        }
-
-        public ISkill RegisterParameterSelector(Define.ActorParameterType type, Func<Actor, Actor, float> selector)
-        {
-            parameterOwnerTargetSelectors[type] = selector;
-            return this;
+            return Mathf.FloorToInt(GetParameter(type, owner));
         }
 
         public ISkill RegisterParameterSelector(Define.ActorParameterType type, Func<Actor, float> selector)
         {
-            parameterOwnerOnlySelectors[type] = selector;
+            parameterSelectors[type] = selector;
             return this;
         }
     }
