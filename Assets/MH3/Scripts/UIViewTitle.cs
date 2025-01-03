c71d5e7356b5a1ab55e50ed062bc6c6480cd0497
gif-hara <hk.mp5k.a4.pdw83@gmail.com>
2025-01-04 02:02:36 +0900

    Refactor OpenAsync method in UIViewTitle to use linked cancellation tokens for improved scope management

--- Assets/MH3/Scripts/UIViewTitle.cs
+++ Assets/MH3/Scripts/UIViewTitle.cs
@@ -10,8 +10,9 @@ namespace MH3
 {
     public class UIViewTitle
     {
-        public static async UniTask OpenAsync(HKUIDocument documentPrefab, CancellationToken cancellationToken)
+        public static async UniTask OpenAsync(HKUIDocument documentPrefab, CancellationToken scope)
         {
+            var newScope = CancellationTokenSource.CreateLinkedTokenSource(scope);
             var document = Object.Instantiate(documentPrefab);
             var inputController = TinyServiceLocator.Resolve<InputController>();
             var areaTitleCanvasGroup = document.Q<CanvasGroup>("Area.Title");
@@ -30,24 +31,26 @@ namespace MH3
                     var (document, inputController) = t;
                     document.Q<TMP_Text>("Text.PressButton").text = string.Format("{0}:ゲームスタート", InputSprite.GetTag(inputController.Actions.UI.Submit)).Localized();
                 })
-                .RegisterTo(cancellationToken);
+                .RegisterTo(newScope.Token);
             gameEvents.OnBeginTitle.OnNext(Unit.Default);
             await TinyServiceLocator.Resolve<UIViewTransition>()
                 .Build()
                 .SetMaterial("Transition.3")
                 .BeginAsync(LMotion.Create(1.0f, 0.0f, 0.5f));
-            await areaTitleAnimation.PlayAsync("In", cancellationToken);
-            await areaPressButtonAnimation.PlayAsync("In", cancellationToken);
+            await areaTitleAnimation.PlayAsync("In", newScope.Token);
+            await areaPressButtonAnimation.PlayAsync("In", newScope.Token);
             areaPressButtonAnimation.Play("Loop");
-            await inputController.Actions.UI.Submit.OnPerformedAsObservable().FirstAsync(cancellationToken);
+            await inputController.Actions.UI.Submit.OnPerformedAsObservable().FirstAsync(newScope.Token);
             gameCameraController.EndTitle();
             await UniTask.WhenAll(
-                areaTitleAnimation.PlayAsync("Out", cancellationToken),
-                areaPressButtonAnimation.PlayAsync("Out", cancellationToken)
+                areaTitleAnimation.PlayAsync("Out", newScope.Token),
+                areaPressButtonAnimation.PlayAsync("Out", newScope.Token)
             );
             gameEvents.OnEndTitle.OnNext(Unit.Default);
             inputController.PopActionType();
             document.DestroySafe();
+            newScope.Cancel();
+            newScope.Dispose();
         }
     }
 }
