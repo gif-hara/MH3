d332aff28a91e9a682bcf7fbe510d976f888c49b
gif-hara <hk.mp5k.a4.pdw83@gmail.com>
2025-01-04 23:05:44 +0900

    Add TryDodge sequence implementation for improved action handling

---
+++ Assets/MH3/Scripts/UnitySequencerSystem/Sequences/TryDodge.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Threading;
+using Cysharp.Threading.Tasks;
+using MH3.UnitySequencerSystem.Resolvers;
+using UnityEngine;
+using UnitySequencerSystem;
+using UnitySequencerSystem.Resolvers;
+
+namespace MH3
+{
+    [Serializable]
+    public class TryDodge : Sequence
+    {
+        [SerializeReference, SubclassSelector]
+        private ActorResolver actorResolver;
+
+        [SerializeReference, SubclassSelector]
+        private StringResolver isSuccessKeyResolver;
+
+        public override UniTask PlayAsync(Container container, CancellationToken cancellationToken)
+        {
+            var actor = actorResolver.Resolve(container);
+            var isSuccess = actor.ActionController.TryDodge();
+            if (isSuccessKeyResolver != null)
+            {
+                container.RegisterOrReplace(isSuccessKeyResolver.Resolve(container), isSuccess);
+            }
+
+            return UniTask.CompletedTask;
+        }
+    }
+}
