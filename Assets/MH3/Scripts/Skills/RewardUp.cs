c6b7343b214b8878c97d00fb992f0ae47da3b5e1
gif-hara <hk.mp5k.a4.pdw83@gmail.com>
2025-01-04 11:45:05 +0900

    Add RewardUp skill to enhance reward capabilities for actors

---
+++ Assets/MH3/Scripts/Skills/RewardUp.cs
@@ -0,0 +1,21 @@
+using HK;
+using MH3.ActorControllers;
+using UnityEngine;
+
+namespace MH3.SkillSystems
+{
+    public class RewardUp : Skill
+    {
+        public RewardUp(int level) : base(Define.SkillType.RewardUp, level)
+        {
+        }
+
+        public override void Attach(Actor owner)
+        {
+            owner.SpecController.RewardUp.RegisterAdds(
+                "Skill.RewardUp",
+                () => Mathf.FloorToInt(TinyServiceLocator.Resolve<MasterData>().SkillRewardUp.Get(Level).Value)
+                );
+        }
+    }
+}
