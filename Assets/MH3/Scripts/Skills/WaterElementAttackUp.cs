1b81f731fc6e5c7d4b0823cd5ef383c2f0aa531c
gif-hara <hk.mp5k.a4.pdw83@gmail.com>
2025-01-04 11:23:31 +0900

    Add WaterElementAttackUp skill implementation

---
+++ Assets/MH3/Scripts/Skills/WaterElementAttackUp.cs
@@ -0,0 +1,26 @@
+using HK;
+using MH3.ActorControllers;
+using UnityEngine;
+
+namespace MH3.SkillSystems
+{
+    public class WaterElementAttackUp : Skill
+    {
+        public WaterElementAttackUp(int level) : base(Define.SkillType.WaterElementAttackUp, level)
+        {
+        }
+
+        public override void Attach(Actor owner)
+        {
+            owner.SpecController.Attack.RegisterAdds(
+                "Skill.WaterElementAttackUp",
+                () =>
+                {
+                    return owner.SpecController.ElementAttackType == Define.ElementType.Water
+                        ? Mathf.FloorToInt(TinyServiceLocator.Resolve<MasterData>().SkillElementAttackUp.Get(Level).Value)
+                        : 0;
+                }
+                );
+        }
+    }
+}
