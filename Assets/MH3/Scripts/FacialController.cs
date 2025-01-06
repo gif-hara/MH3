4277e6d3e83305db0d37cfe4b6a550ac028ae1a6
gif-hara <hk.mp5k.a4.pdw83@gmail.com>
2025-01-07 00:25:10 +0900

    Add facial animation clips and enhance FacialController functionality

--- Assets/MH3/Scripts/FacialController.cs
+++ Assets/MH3/Scripts/FacialController.cs
@@ -1,19 +1,41 @@
+using System;
+using System.Collections.Generic;
 using UnityEngine;
 
 namespace MH3
 {
     public class FacialController : MonoBehaviour
     {
-        // Start is called once before the first execution of Update after the MonoBehaviour is created
+        [SerializeField]
+        private List<Element> elements;
+
+        [SerializeField]
+        private string defaultFacialName;
+
         void Start()
         {
-        
+            SetFacial(defaultFacialName);
+            Debug.Log("FacialController.Start() called");
         }
 
-        // Update is called once per frame
-        void Update()
+        public void SetFacial(string facialName)
         {
-        
+            foreach (var element in elements)
+            {
+                element.SetActive(facialName);
+            }
+        }
+
+        [Serializable]
+        public class Element
+        {
+            [SerializeField]
+            private GameObject facialObject;
+
+            public void SetActive(string facialName)
+            {
+                facialObject.SetActive(facialObject.name == facialName);
+            }
         }
     }
 }
