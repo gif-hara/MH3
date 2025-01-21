c653af979dfc3cd783aefcbdf8ac0e08567b4430
gif-hara <hk.mp5k.a4.pdw83@gmail.com>
2025-01-22 00:14:25 +0900

    Add ActorTriggerObserve sequence for actor state observation and triggering

---
+++ Assets/MH3/Scripts/UnitySequencerSystem/Sequences/ActorTriggerObserve.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Threading;
+using Cysharp.Threading.Tasks;
+using MH3.ActorControllers;
+using MH3.UnitySequencerSystem.Resolvers;
+using R3;
+using UnityEngine;
+using UnitySequencerSystem;
+using UnitySequencerSystem.Resolvers;
+
+namespace MH3
+{
+    [Serializable]
+    public class ActorTriggerObserve : Sequence
+    {
+        [SerializeReference, SubclassSelector]
+        private ActorResolver actorResolver;
+
+        [SerializeField]
+        private ActorStateProvider.TriggerType triggerType;
+
+        [SerializeReference, SubclassSelector]
+        private SequencesResolver sequences;
+
+        public override UniTask PlayAsync(Container container, CancellationToken cancellationToken)
+        {
+            var actor = actorResolver.Resolve(container);
+            actor.StateProvider.GetTriggerAsObservable(triggerType)
+                .Subscribe((actor, this, cancellationToken, container), async static (_, t) =>
+                {
+                    var (actor, self, cancellationToken, container) = t;
+                    var sequencer = new Sequencer(container, self.sequences.Resolve(container));
+                    await sequencer.PlayAsync(cancellationToken);
+                })
+                .RegisterTo(actor.destroyCancellationToken);
+            return UniTask.CompletedTask;
+        }
+    }
+}
