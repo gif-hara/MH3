bfaa24a260ce2e8a3ea78fc6d5778ba9f68e5b57
gif-hara <hk.mp5k.a4.pdw83@gmail.com>
2025-02-06 01:28:42 +0900

    Add UI acquire reward audio clips and associated metadata

--- Assets/MH3/Scripts/UIViewAcqureReward.cs
+++ Assets/MH3/Scripts/UIViewAcqureReward.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading;
@@ -22,20 +23,21 @@ namespace MH3
                 return -1;
             }
 
-            var document = Object.Instantiate(documentPrefab);
+            var document = UnityEngine.Object.Instantiate(documentPrefab);
             var elementParent = document.Q<RectTransform>("Parent.Element");
             var selectable = new List<Selectable>();
             var source = new UniTaskCompletionSource<int>();
             document.Q<TMP_Text>("EnemyName").text = enemyName;
             var timeSpan = System.TimeSpan.FromSeconds(elapsedTime);
             document.Q<TMP_Text>("ClearTime").text = $"{timeSpan.Minutes:D2}:{timeSpan.Seconds:D2}:{timeSpan.Milliseconds:D3}";
+            var selectScope = new CancellationTokenSource();
             foreach (var reward in rewards)
             {
                 switch (reward)
                 {
                     case InstanceWeapon instanceWeapon:
                         {
-                            var element = Object.Instantiate(document.Q<HKUIDocument>("Prefab.InstanceWeapon"), elementParent);
+                            var element = UnityEngine.Object.Instantiate(document.Q<HKUIDocument>("Prefab.InstanceWeapon"), elementParent);
                             var container = new Container();
                             container.Register("InstanceWeapon", instanceWeapon);
                             element.Q<SequencesMonoBehaviour>("Sequences").PlayAsync(container, scope).Forget();
@@ -44,14 +46,15 @@ namespace MH3
                                 .Subscribe(_ =>
                                 {
                                     source.TrySetResult(selectable.IndexOf(button));
+                                    PlayAcquireEffect(element);
                                 })
-                                .RegisterTo(document.destroyCancellationToken);
+                                .RegisterTo(selectScope.Token);
                             selectable.Add(button);
                             break;
                         }
                     case InstanceSkillCore instanceSkillCore:
                         {
-                            var element = Object.Instantiate(document.Q<HKUIDocument>("Prefab.InstanceSkillCore"), elementParent);
+                            var element = UnityEngine.Object.Instantiate(document.Q<HKUIDocument>("Prefab.InstanceSkillCore"), elementParent);
                             var container = new Container();
                             container.Register("InstanceSkillCore", instanceSkillCore);
                             element.Q<SequencesMonoBehaviour>("Sequences").PlayAsync(container, scope).Forget();
@@ -60,14 +63,15 @@ namespace MH3
                                 .Subscribe(_ =>
                                 {
                                     source.TrySetResult(selectable.IndexOf(button));
+                                    PlayAcquireEffect(element);
                                 })
-                                .RegisterTo(document.destroyCancellationToken);
+                                .RegisterTo(selectScope.Token);
                             selectable.Add(button);
                             break;
                         }
                     case InstanceArmor instanceArmor:
                         {
-                            var element = Object.Instantiate(document.Q<HKUIDocument>("Prefab.InstanceArmor"), elementParent);
+                            var element = UnityEngine.Object.Instantiate(document.Q<HKUIDocument>("Prefab.InstanceArmor"), elementParent);
                             var container = new Container();
                             container.Register("InstanceArmor", instanceArmor);
                             element.Q<SequencesMonoBehaviour>("Sequences").PlayAsync(container, scope).Forget();
@@ -76,8 +80,9 @@ namespace MH3
                                 .Subscribe(_ =>
                                 {
                                     source.TrySetResult(selectable.IndexOf(button));
+                                    PlayAcquireEffect(element);
                                 })
-                                .RegisterTo(document.destroyCancellationToken);
+                                .RegisterTo(selectScope.Token);
                             selectable.Add(button);
                             break;
                         }
@@ -87,15 +92,33 @@ namespace MH3
             }
 
             selectable.SetNavigationHorizontal();
-
             var inputController = TinyServiceLocator.Resolve<InputController>();
             inputController.PushActionType(InputController.InputActionType.UI);
             var defaultSelectable = selectable.First();
             EventSystem.current.SetSelectedGameObject(defaultSelectable.gameObject);
             var result = await source.Task;
+            selectScope.Cancel();
+            selectScope.Dispose();
+            foreach (var i in selectable)
+            {
+                i.navigation = new Navigation();
+            }
+
+            // 獲得エフェクトアニメーションが終わるまで待つ
+            await UniTask.Delay(TimeSpan.FromSeconds(2.0f), cancellationToken: scope);
+
             inputController.PopActionType();
             document.DestroySafe();
             return result;
+
+            static void PlayAcquireEffect(HKUIDocument element)
+            {
+                var effect = element.Q<HKUIDocument>("AcquireEffect");
+                effect.gameObject.SetActive(true);
+                effect
+                    .Q<SimpleAnimation>("SimpleAnimation")
+                    .Play("Default");
+            }
         }
     }
 }
