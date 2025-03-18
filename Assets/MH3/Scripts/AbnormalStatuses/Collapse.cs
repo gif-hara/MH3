223db168b4f97158ca22f3f6407547e3936547ad
gif-hara <hk.mp5k.a4.pdw83@gmail.com>
2025-03-18 23:16:40 +0900

    Add Collapse effect prefab and implement status application logic for enhanced gameplay mechanics

--- Assets/MH3/Scripts/AbnormalStatuses/Collapse.cs
+++ Assets/MH3/Scripts/AbnormalStatuses/Collapse.cs
@@ -1,4 +1,5 @@
 using Cysharp.Threading.Tasks;
+using HK;
 using MH3.ActorControllers;
 
 namespace MH3.AbnormalStatusSystems
@@ -7,8 +8,16 @@ namespace MH3.AbnormalStatusSystems
     {
         public override async UniTaskVoid Apply(Actor target)
         {
+            var effectManager = TinyServiceLocator.Resolve<EffectManager>();
+            var (effectObject, pool) = effectManager.RentManual("AbnormalStatus.Collapse.1");
+            var parent = target.LocatorHolder.Get("Spine");
+            effectObject.transform.SetParent(parent);
+            effectObject.transform.localPosition = UnityEngine.Vector3.zero;
+            effectObject.transform.localRotation = UnityEngine.Quaternion.identity;
+            effectObject.transform.localScale = UnityEngine.Vector3.one;
             await UniTask.Delay(System.TimeSpan.FromSeconds(target.SpecController.CollapseDuration), cancellationToken: target.destroyCancellationToken);
             target.SpecController.RemoveAppliedAbnormalStatus(Define.AbnormalStatusType.Collapse);
+            effectManager.Return(effectObject, pool);
         }
     }
 }
